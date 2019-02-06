b631a68ecebe0dfdf980702b1416b5f27df1772c
Denis Korolev <admin@leprosus.ru>
2019-02-06 15:06:59 +0200

    Add: RC of balancer

---
+++ balancer.go
@@ -0,0 +1,126 @@
+package golang_balancer
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Balancer struct {
+	countPerSecond int32
+	min            int32
+	max            int32
+	efficiency     int32
+
+	handler func(job interface{}) (err error)
+
+	err chan error
+
+	wg sync.WaitGroup
+}
+
+func NewBalancer(jobCh chan interface{}, handler func(job interface{}) (err error), errChan chan error, countPerSecond int32) (b *Balancer) {
+	b = &Balancer{
+		handler:        handler,
+		countPerSecond: countPerSecond,
+		max:            2 * countPerSecond,
+		min:            0,
+
+		err: errChan,
+	}
+
+	var counter, efficiency int32
+	ticker := time.NewTicker(time.Second)
+
+	go func() {
+		for range ticker.C {
+			efficiency = atomic.LoadInt32(&counter)
+			atomic.StoreInt32(&counter, 0)
+
+			atomic.StoreInt32(&b.efficiency, efficiency)
+		}
+	}()
+
+	go func() {
+		var (
+			err     error
+			sleep   time.Duration
+			current int32
+		)
+
+		for job := range jobCh {
+			b.wg.Add(1)
+
+			go func() {
+				err = handler(job)
+				if err != nil {
+					b.err <- err
+				}
+
+				b.wg.Done()
+
+				atomic.AddInt32(&counter, 1)
+			}()
+
+			current = atomic.LoadInt32(&b.countPerSecond)
+
+			sleep = time.Second / time.Duration(current)
+
+			time.Sleep(sleep)
+		}
+	}()
+
+	return
+}
+
+func (b *Balancer) Max(max int32) (ok bool) {
+	if 2*atomic.LoadInt32(&b.countPerSecond) < max {
+		return
+	}
+
+	atomic.StoreInt32(&b.max, max)
+
+	return true
+}
+
+func (b *Balancer) Min(min int32) (ok bool) {
+	if 2*atomic.LoadInt32(&b.countPerSecond) < 0 {
+		return
+	}
+
+	atomic.StoreInt32(&b.min, min)
+
+	return true
+}
+
+func (b *Balancer) Increase() (ok bool) {
+	if atomic.LoadInt32(&b.countPerSecond) >= atomic.LoadInt32(&b.max) {
+		return
+	}
+
+	atomic.AddInt32(&b.countPerSecond, 1)
+
+	return true
+}
+
+func (b *Balancer) Decrease() (ok bool) {
+	if atomic.LoadInt32(&b.countPerSecond) <= atomic.LoadInt32(&b.min) {
+		return
+	}
+
+	atomic.AddInt32(&b.countPerSecond, -1)
+
+	return true
+}
+
+func (b *Balancer) CountPerSecond() (countPerSecond int32) {
+	return atomic.LoadInt32(&b.countPerSecond)
+}
+
+func (b *Balancer) Efficiency() (efficiency int32) {
+	return atomic.LoadInt32(&b.efficiency)
+}
+
+func (b *Balancer) Wait() {
+	b.wg.Wait()
+}
---
+++ balancer_test.go
@@ -0,0 +1,129 @@
+package golang_balancer
+
+import (
+	"github.com/pkg/errors"
+	"testing"
+	"time"
+)
+
+func getInstance() (b *Balancer) {
+	var (
+		ch      = make(chan interface{})
+		handler = func(job interface{}) (err error) {
+			println(job.(string))
+
+			return
+		}
+		err = make(chan error)
+	)
+
+	b = NewBalancer(ch, handler, err, 10)
+
+	return
+}
+
+func TestInit(t *testing.T) {
+	b := getInstance()
+
+	if b.CountPerSecond() != 10 {
+		t.Fatal("Can't init balancer correctly")
+	}
+}
+
+func TestIncrease(t *testing.T) {
+	b := getInstance()
+
+	b.Increase()
+	b.Increase()
+
+	if b.CountPerSecond() != 12 {
+		t.Fatal("Can't increase amount goroutins in balancer")
+	}
+}
+
+func TestDecrease(t *testing.T) {
+	b := getInstance()
+
+	b.Decrease()
+	b.Decrease()
+
+	if b.CountPerSecond() != 8 {
+		t.Fatal("Can't decrease amount goroutins in balancer")
+	}
+}
+
+func TestIncreaseWithLimitation(t *testing.T) {
+	b := getInstance()
+
+	b.Max(11)
+	b.Increase()
+	b.Increase()
+
+	if b.CountPerSecond() != 11 {
+		t.Fatal("Can't increase (with limitation) amount goroutins in balancer")
+	}
+}
+
+func TestDecreaseWithLimitation(t *testing.T) {
+	b := getInstance()
+
+	b.Min(9)
+	b.Decrease()
+	b.Decrease()
+
+	if b.CountPerSecond() != 9 {
+		t.Fatal("Can't decrease (with limitation) amount goroutins in balancer")
+	}
+}
+
+func TestErrChan(t *testing.T) {
+	var (
+		ch      = make(chan interface{})
+		handler = func(job interface{}) (err error) {
+			return errors.New("some error")
+		}
+
+		err = make(chan error)
+	)
+
+	NewBalancer(ch, handler, err, 10)
+
+	ch <- "some job"
+
+	time.AfterFunc(5*time.Second, func() {
+		t.Fatal("Can't get error from channel")
+	})
+
+	<-err
+}
+
+func TestCountPerSecond(t *testing.T) {
+	b := getInstance()
+
+	if b.CountPerSecond() != 10 {
+		t.Fatal("Can't get expected count per second value")
+	}
+}
+
+func TestEfficiency(t *testing.T) {
+	var (
+		ch      = make(chan interface{})
+		handler = func(job interface{}) (err error) {
+			return
+		}
+
+		err = make(chan error)
+	)
+
+	b := NewBalancer(ch, handler, err, 10)
+
+	time.AfterFunc(1100*time.Millisecond, func() {
+		if b.Efficiency() != 10 {
+			t.Fatal("Can't balance necessary amount jobs execution per second")
+		}
+	})
+
+	for i := 0; i < 12; i++ {
+		ch <- "some job"
+	}
+}
